bada49296531fdcf6ccec9af7054b47f0c75b819
AR04 <anjanraisam04@gmail.com>
2025-07-03 21:04:15 +0530

    site_config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astronaut.github.io',
+  site: 'https://04ar.github.io/',
   // base: 'portfolio_site',
   integrations: [mdx(), sitemap()],
 

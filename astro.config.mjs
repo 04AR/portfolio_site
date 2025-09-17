1e2040b5fff2500990049991f90a26fa5600f9a8
AR04 <anjalraisam04@gmail.com>
2025-09-18 01:12:24 +0530

    astro_configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://04ar.github.io/',
-  base: 'portfolio_site',
+  site: "https://04ar.github.io/",
+  base: "portfolio_site",
   integrations: [mdx(), sitemap()],
 
   vite: {
